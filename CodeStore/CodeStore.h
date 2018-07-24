00c72450a022fc1270f9cfacf512b2855b16a30b
王浩 <wh@addcn.com>
2018-07-24 12:00:22 +0800

    first commit

---
+++ CodeStore/CodeStore.h
@@ -0,0 +1,19 @@
+//
+//  CodeStore.h
+//  CodeStore
+//
+//  Created by onewh on 2018/7/24.
+//  Copyright © 2018年 onewh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CodeStore.
+FOUNDATION_EXPORT double CodeStoreVersionNumber;
+
+//! Project version string for CodeStore.
+FOUNDATION_EXPORT const unsigned char CodeStoreVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CodeStore/PublicHeader.h>
+
+
