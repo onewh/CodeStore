50a3a7b78e9010a8823414aaccd0f2d8dadfb0a7
王浩 <wh@addcn.com>
2018-07-24 16:36:18 +0800

    add file

--- CodeStore.podspec
+++ CodeStore.podspec
@@ -79,7 +79,7 @@ Pod::Spec.new do |s|
   #  Supports git, hg, bzr, svn and HTTP.
   #
 
-  s.source       = { :git => "https://github.com/onewh/CodeStore.git", :tag => "#{0.0.2}" }
+  s.source       = { :git => "https://github.com/onewh/CodeStore.git", :tag => "#{s.version}" }
 
 
   # ――― Source Code ―――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――― #
---
+++ CodeStore/AppInfoUtility.swift
@@ -0,0 +1,19 @@
+//
+//  AppInfoUtility.swift
+//  CodeStore
+//
+//  Created by onewh on 2018/7/24.
+//  Copyright © 2018年 onewh. All rights reserved.
+//
+
+import UIKit
+
+class AppInfoUtility: NSObject {
+
+    @objc public static func appVersion() -> String {
+        if let ver = Bundle.main.infoDictionary?["CFBundleShortVersionString"] as? String {
+            return ver
+        }
+        return ""
+    }
+}
